49eb9b6827fad0ed8994c47c63067a31409213ea
Marina-Zafiris <mlzfiris@gmail.com>
2020-08-04 16:10:54 -0500

    Update Pansharpen_Images.py
    Update Random Subset Function

--- Pansharpen_Images.py
+++ Pansharpen_Images.py
@@ -10,16 +10,18 @@ from skimage.color import rgb2hsv, hsv2rgb
 
 # define the file names for RGB and panchromatic (pan) images
 # iterate or randomize through ID numbers in future
-file_rgb = "./data/outputs/RGB_LC08_L1TP_109076_20170521_20170526_01_T1.tif"
-file_pan = "./data/LC08_L1TP_109076_20170521_20170526_01_T1/LC08_L1TP_109076_20170521_20170526_01_T1_B8.TIF"
+file_rgb = "./data/RGB_LC08_L1TP_109076_20170521_20170526_01_T1.tif"
+file_pan = "./data/LC08_L1TP_109076_20170521_20170526_01_T1_B8.TIF"
+file_qa = "./data/LC08_L1TP_109076_20170521_20170526_01_T1_BQA.TIF"
 
 # load the data
 try:
     data_rgb = gdal.Open(file_rgb, GA_ReadOnly)
     data_pan = gdal.Open(file_pan, GA_ReadOnly) 
+    data_qa = gdal.Open(file_qa, GA_ReadOnly) 
 except:
     print('error loading files')
-    
+
 # Initialize empty RGB array
 columns = dataset_rgb.RasterXSize
 rows = dataset_rgb.RasterYSize
@@ -30,6 +32,7 @@ rgb[:,:,0] = data_rgb.GetRasterBand(3).ReadAsArray().astype(float)
 rgb[:,:,1] = data_rgb.GetRasterBand(2).ReadAsArray().astype(float)
 rgb[:,:,2] = data_rgb.GetRasterBand(1).ReadAsArray().astype(float)
 pan = data_pan.GetRasterBand(1).ReadAsArray().astype(float)
+qa = data_qa.GetRasterBand(1).ReadAsArray().astype(float)
 
 # max pixel to normalize values
 max_pix = 26032.0 #np.max(array_rgb)
@@ -42,17 +45,46 @@ array_pan = pan / max_pix
 columns = data_pan.RasterXSize
 rows = data_pan.RasterYSize
 array_rgb = resize(array_rgb, (rows, columns))
+array_qa = resize(qa, (rows, columns)) # Necessary?
 
+# Checking the resize and normalization
 # Checking the resize and normalization
 print(np.shape(array_rgb))
 print(np.shape(array_pan))
+print(np.shape(array_qa))
 print(np.max(array_rgb))
 print(np.max(array_pan))
+print(np.max(array_qa))
 
 #Making subset
 #When we need random subset can be rand.int in interval
 sub_array_rgb = array_rgb[9500:10000, 9500:10000, :]
 sub_array_pan = array_pan[9500:10000, 9500:10000]
+
+# RANDOM SUBSET FUNCTION
+import random
+
+def random_subset(x, y):
+    high_x = 15561-x
+    high_y = 15381-y
+    x1 = numpy.random.randint(0, high=high_x, size=None, dtype='l')
+    x2 = x1+x
+    y1 = numpy.random.randint(0, high=high_y, size=None, dtype='l')
+    y2 = y1+y
+    sub_array_qa = array_qa[x1:x2, y1:y2]
+    values_qa = list(np.sort(np.unique(sub_array_qa)))
+    values_pass = [2720, 2724, 2738, 2732]
+    while values_qa == values_pass is False: #Need to make into filter(lambda) function w range?
+        high_x = 15561-x
+        high_y = 15381-y
+        x1 = numpy.random.randint(0, high=high_x, size=None, dtype='l')
+        x2 = x1+x
+        y1 = numpy.random.randint(0, high=high_y, size=None, dtype='l')
+        y2 = y1+y
+        sub_array_qa = array_qa[x1:x2, y1:y2]
+        values_qa = list(np.sort(np.unique(sub_array_qa)))
+        values_pass = [2720, 2724, 2738, 2732]
+
 
 # HSV to RGB transform
 array_hsv = rgb2hsv(sub_array_rgb)
